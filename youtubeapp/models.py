5d866bc151df96814b9ae7eabcc1f33b2c629283
tharun323 <atkr323@gmail.com>
2019-03-04 17:07:57 +0530

    fix errors in rest urls

--- youtubeapp/models.py
+++ youtubeapp/models.py
@@ -5,16 +5,18 @@ class ChannelMaster(models.Model):
     channel_name=models.CharField(default=None,max_length=200)
 
 class ChannelStats(models.Model):
+    channel_master = models.ForeignKey('ChannelMaster', on_delete=models.CASCADE)
     total_views=models.IntegerField(default=None)
     time_stamp=models.DateTimeField(auto_now=True)
-    channel_master=models.ForeignKey(ChannelMaster,on_delete=models.CASCADE)
+
 
 class VideoMaster(models.Model):
+    channel_master = models.ForeignKey('ChannelMaster', on_delete=models.CASCADE)
     video_id=models.CharField(default=None,max_length=200)
     video_name=models.CharField(default=None,max_length=200)
-    channel_master=models.ForeignKey(ChannelMaster,on_delete=models.CASCADE)
+
 
 class VideoStats(models.Model):
+    video_master = models.ForeignKey('VideoMaster', on_delete=models.CASCADE)
     total_views=models.IntegerField(default=None)
     time_stamp=models.DateTimeField(auto_now=True)
-    video_master=models.ForeignKey(VideoMaster,on_delete=models.CASCADE)
--- youtubeapp/urls.py
+++ youtubeapp/urls.py
@@ -4,11 +4,10 @@ from rest_framework import routers, serializers, viewsets
 from . views import *
 from . import views
 router = routers.DefaultRouter()
-print("I am in urls \n\n\n\n")
 router.register(r'ChannelMasterViewSet', views.ChannelMasterViewSet, base_name='ChannelMaster')
-router.register(r'ChannelStatsViewSet',views.ChannelStats,base_name='ChannelStats')
-router.register(r'VideoMasterViewSet',views.VideoMaster,base_name='VideoMaster')
-router.register(r'VideoStatsViewSet',views.VideoStats,base_name='VideoStats')
+router.register(r'ChannelStatsViewSet',views.ChannelStatsViewSet,base_name='ChannelStats')
+router.register(r'VideoMasterViewSet',views.VideoMasterViewSet,base_name='VideoMaster')
+router.register(r'VideoStatsViewSet',views.VideoStatsViewSet,base_name='VideoStats')
 
 
 urlpatterns = [
